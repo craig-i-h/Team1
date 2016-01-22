ed6a6d6554b5b35c17a3761e84ce3025a5bd0904
coleycombie <nicole.mccombie@atos.net>
2016-01-22 10:22:01 +0000

    [Task99557, repository layer created for asset component, null]

---
+++ svc/public/webapp/service/repository/asset.js
@@ -0,0 +1,72 @@
+"use strict";
+
+(function () {
+
+
+    angular.module("app").service("assetRepository", ["$q", "$log", "assetDal", AssetRepo]);
+
+    function AssetRepo($q, $log, assetDal) {
+
+        var assetCache = [];
+        console.log("This is asset cache");
+
+
+        this.getAssets = function (criteria) {
+            $log.debug("Repository:Asset getAsset");
+
+            var deferred = $q.defer();
+            assetDal.getAsset(criteria).then(function (results) {
+
+                assetCache = results;
+                deferred.resolve(results);
+            }, function (error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+
+        this.saveAsset = function (assetToSave) {
+            $log.debug("Repository:Asset - saveAsset");
+
+            var deferred = $q.defer();
+            var isUpdate = assetToSave.hasOwnProperty("id");
+
+
+            $log.debug("isUpdate = " + isUpdate);
+            $log.debug(JSON.stringify(assetToSave));
+
+            passetDal.saveAsset(assetToSave).then(function (asset) {
+
+                if (!isUpdate) {
+                    assetCache.push(asset);
+                }
+                deferred.resolve(asset);
+            }, function (error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        this.deleteAsset = function (assetToDelete) {
+            $log.debug("Repository:Asset - deleteAsset");
+
+            var deferred = $q.defer();
+            assetDal.deleteAsset(assetToDelete).then(function (assets) {
+                _.remove(assetCache, {
+                    id: assetToDelete.id
+                });
+
+                deferred.resolve(assetCache);
+            }, function (error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        $log.debug("Repository:Asset Instantiated");
+    }
+}());
